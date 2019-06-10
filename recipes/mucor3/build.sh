31487ba06b6a8e720b7e7d90464e7ac4c761405d
Charles Gregory <charles.gregory@osumc.edu>
2019-06-10 15:13:10 -0400

    build and tests working

--- recipes/mucor3/build.sh
+++ recipes/mucor3/build.sh
@@ -9,5 +9,5 @@ cd ..
 cd depthGauge
 ldc2 -L-lhts -O2 -release -of depthgauge source/* dhtslib/source/dhtslib/*.d dhtslib/source/dhtslib/htslib/*.d
 cd ..
-python setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
 cp depthGauge/depthgauge vcf_atomizer/bin/atomizer $PREFIX/bin
