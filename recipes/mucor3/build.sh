f0eb410c8b4c71be897489cbeb6fe12f5ce49031
Charles Gregory <charles.gregory@osumc.edu>
2019-06-14 16:02:34 -0400

    updated shas and links for v0.1

--- recipes/mucor3/build.sh
+++ recipes/mucor3/build.sh
@@ -3,11 +3,10 @@ set -eu -o pipefail
 
 cd "${SRC_DIR}"
 cd vcf_atomizer
-make deps
-make build
+make
 cd ..
 cd depthGauge
 ldc2 -L-lhts -O2 -release -of depthgauge source/* dhtslib/source/dhtslib/*.d dhtslib/source/dhtslib/htslib/*.d
 cd ..
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-cp depthGauge/depthgauge vcf_atomizer/bin/atomizer $PREFIX/bin
+cp depthGauge/depthgauge vcf_atomizer/bin/vcf_atomizer $PREFIX/bin
